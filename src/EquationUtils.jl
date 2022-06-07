eea6ef8678e73b1dd2222b6f4a68c33c7320090a
Charles Fox <charfox1@umbc.edu>
2022-06-07 18:36:47 -0400

    Revert "new version of thermo constraints"
    This reverts commit 77da654e67ca0d8bbcc7a05773cce9b5ed56d280.

--- src/EquationUtils.jl
+++ src/EquationUtils.jl
@@ -106,72 +106,83 @@ end
 # custom function to check if expression is monontonically increasing
 py"""
 import sympy
-def is_monotonic_increasing_test(expr, interval, var):
-
-    # constant value never decreases    
-    if expr.is_constant():
-        return True
-
+def is_monotonic_increasing(expr, interval, var):
     # get critical points as list
     turning_points = list(sympy.solveset(expr.diff(var), var, interval))
     turning_points.sort()
+    print("turning points =", turning_points)
+
     # failed to find critical points
     # there could be 0 or infinite...
     if (turning_points == []):
         # fall back to simpler increasing function
-        return bool(1 if (expr.limit(var, interval.end) - expr.limit(var, interval.start)) >= 0 else 0)
+        return sympy.is_increasing(expr, sympy.Interval(0,sympy.oo), var)
+
     increasing = 1
+
+    #print("turning points = ", turning_points)
     # turn to false if interval from start of main interval to first critical point not increasing
-    increasing = min(increasing, (1 if (expr.limit(var, turning_points[0]) - expr.limit(var, interval.start)) >= 0 else 0))
+    increasing = min(increasing, sympy.is_increasing(expr, sympy.Interval(interval.start, turning_points[0]), var))
     # check intervals between all critical points
     for i in range(len(turning_points)-1):
         thisPoint = turning_points[i]
         nextPoint = turning_points[i+1]
-        increasing = min(increasing, (1 if (expr.limit(var, nextPoint) - expr.limit(var, thisPoint)) >= 0 else 0))
-        #increasing = min(increasing, sympy.is_increasing(expr, sympy.Interval(thisPoint, nextPoint, false, false), var))
+        increasing = min(increasing, sympy.is_increasing(expr, sympy.Interval(thisPoint, nextPoint), var))
     # check last interval
-    increasing = min(increasing, (1 if (expr.limit(var, interval.end) - expr.limit(var, turning_points[-1])) >= 0 else 0))
-    #increasing = min(increasing, sympy.is_increasing(expr, sympy.Interval(turning_points[-1], interval.end, false, false), var))
+    increasing = min(increasing, sympy.is_increasing(expr, sympy.Interval(turning_points[-1], interval.end), var))
     return bool(increasing)
 """
 
+# no parameters to fill variant
 function thermoConstraints(expr::PyObject, var::PyObject)
 
+    # don't do this here :(
+    #sympy = pyimport("sympy")
+    #var = sympy.symbols("p")
+    #expr = sympy.parse_expr(expr)
+
+    #println("expression:", expr)
     results = [true, true, true]
+
     
     # Axiom 1: the expr needs to pass through the origin
     try
         if sympy.limit(expr, var, 0, "+") != 0
             #println("constraint 1")
-            results[1] = False
+            results[1] = false
         end
     catch error
         #println(error)
         #println("SymPy cannot evaluate Axiom 1")
-        results[1] = False
+        results[1] = false
+    end
     # Axiom 2: the expr needs to converge to Henry's Law at zero pressure
     try
         if (sympy.limit(sympy.diff(expr, var), var, 0) == sympy.oo 
             || sympy.limit(sympy.diff(expr, var), var, 0) == -sympy.oo 
             || sympy.limit(sympy.diff(expr, var), var, 0) == 0)
             #println("constraint 2")
-            results[2] = False
+            results[2] = false
         end
     catch error
         #println(error)
         #println("SymPy cannot evaluate Axiom 2")
-        results[2] = False
+        results[2] = false
+    end
 
     # Axiom 3: the expr must be strictly increasing as pressure increases
     try
         # use custom function because sympy doesn't work as expected
-        if not(is_monotonic_increasing_test(expr, sympy.Interval(0,sympy.oo), var))
+        if !(py"is_monotonic_increasing"(expr, sympy.Interval(0,sympy.oo), var))
             #println("constraint 3")
-            results[3] = False
+            results[3] = false
         end
     catch error
-        #print("SymPy cannot evaluate Axiom 3")
-        print("Oops!", sys.exc_info()[0], "occurred for", expr)
-        results[3] = False
+        #println("SymPy cannot evaluate Axiom 3")
+        #println(error)
+        results[3] = false
+    end
 
     return results
+
+end
